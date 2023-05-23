de0207e664cfeab80b670d5bc26f6704587f3d9a
PyroUserBot <129763781+PyroUserBot@users.noreply.github.com>
2023-05-23 23:07:07 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -40,7 +40,7 @@ SONG_DOWNLOAD_DURATION = int(
 LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1001661321806"))
 
 # A name for your Music bot.
-MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME","Killer Bee Yo!")
+MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME","Nobara Kugisaki!")
 
 # Your User ID.
 OWNER_ID = list(
