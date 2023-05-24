de1c81fc7a8dd199daadc60313c015c2754456f1
PyroUserBot <129763781+PyroUserBot@users.noreply.github.com>
2023-05-24 07:20:15 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -21,7 +21,7 @@ API_ID = int(getenv("API_ID", "19099900"))
 API_HASH = getenv("API_HASH","2b445de78e5baf012a0793e60bd4fbf5")
 
 ## Get it from @Botfather in Telegram.
-BOT_TOKEN = getenv("BOT_TOKEN", "6295491451:AAFLNsmWCep33iJuU7p_y4-mkHHlR8426DI")
+BOT_TOKEN = getenv("BOT_TOKEN", "6295491451:AAGLNLR57TkbVKgxj4XBR7QHBMqJSvicc_c")
 
 # Database to save your chats and stats... Get MongoDB:-  https://telegra.ph/How-To-get-Mongodb-URI-04-06
 MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://sonu55:sonu55@cluster0.vqztrvk.mongodb.net/?retryWrites=true&w=majority")
