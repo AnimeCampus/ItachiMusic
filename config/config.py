b10cbc638824985b94cbb002ade6dcdaeddb117a
PyroUserBot <129763781+PyroUserBot@users.noreply.github.com>
2023-08-05 23:33:39 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -168,7 +168,10 @@ autoclean = []
 
 
 # Images
-START_IMG_URL = getenv("START_IMG_URL", None)
+START_IMG_URL = getenv(
+    "START_IMG_URL",
+    "assets/Nobara.jpg", 
+)
 
 PING_IMG_URL = getenv(
     "PING_IMG_URL",
